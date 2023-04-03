df25c9da824af9c40918a61ae72c64cb4b040437
liushaohua <shaohua.liu@zhaopin.com.cn>
2023-04-03 14:28:27 +0800

    自定义信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://i.328888.xyz/2023/04/03/iOXMrA.jpeg',
+      name: 'javalsh',
+      description: 'Star on <a href="https://github.com/javalsh" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
